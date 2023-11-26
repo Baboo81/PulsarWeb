ceb6e0d2049ff8d18d7bc63691b3c58a23e3e070
Baboo81 <magicbaboo@gmail.com>
2023-11-26 16:07:41 +0100

    map

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+
 import { link } from "react-router-dom";
 import style from './footer.module.css';
+import { Marker, MapContainer, TileLayer, useMap } from 'react-leaflet'
 
-
+const position = [50.709925, 4.352603];
 const Footer = () => (
 
     <footer className="w-100 text-center">
@@ -10,6 +12,12 @@ const Footer = () => (
                 <div className="col-md-6">
                     <h2>Adresse</h2>
                     <p>14 rue champ rodange, 1410 Waterloo</p>
+                    <MapContainer className= {style.map} center={position} zoom={16.4} scrollWheelZoom={true}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>,
                 </div>
                 <div className="col-md-6">
 
@@ -18,6 +26,8 @@ const Footer = () => (
         </div>
     </footer>
 );
+
+
 
 
 
